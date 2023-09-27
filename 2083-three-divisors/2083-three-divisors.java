acbefc403ab9af22f8caf1fde842c607308e6bbe
Jay <124047536+jaydoshi01@users.noreply.github.com>
2023-09-27 19:01:22 +0530

    Time: 0 ms (100%), Space: 39.1 MB (74.57%) - LeetHub

--- 2083-three-divisors/2083-three-divisors.java
+++ 2083-three-divisors/2083-three-divisors.java
@@ -1,14 +1,37 @@
+// class Solution {
+//     public boolean isThree(int n) {
+//         int count=0;
+//         for(int i=1;i<=n/2;i++)
+//         {
+//             if(n%i==0)
+//             {
+//                 count++;
+//             }
+//         }
+//         count++;
+//         return (count==3)?true:false;
+//     }
+// }
+
 class Solution {
-    public boolean isThree(int n) {
-        int count=0;
-        for(int i=1;i<=n/2;i++)
-        {
-            if(n%i==0)
-            {
+	public boolean isThree(int n) {
+		if (n <= 0) {
+            return false;
+        }
+		  
+        int count = 0;
+
+        for (int i = 1; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
                 count++;
+                if (n / i != i) {
+                    count++;
+                }
+            }
+            if (count > 3) {
+                return false;
             }
         }
-        count++;
-        return (count==3)?true:false;
+        return count == 3;
+	}
     }
-}
