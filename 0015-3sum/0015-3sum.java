b85270ac4559845e015ac9ff9ac6dde0e9481700
Jay <124047536+jaydoshi01@users.noreply.github.com>
2023-10-04 12:11:39 +0530

    Time: 32 ms (56.97%), Space: 50.5 MB (73.36%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,26 +1,74 @@
+// class Solution {
+//     public List<List<Integer>> threeSum(int[] nums) {
+//         int target = 0;
+//         Arrays.sort(nums);
+//         Set<List<Integer>> s = new HashSet<>();
+//         List<List<Integer>> output = new ArrayList<>();
+//         for (int i = 0; i < nums.length; i++){
+//             int j = i + 1;
+//             int k = nums.length - 1;
+//             while (j < k) {
+//                 int sum = nums[i] + nums[j] + nums[k];
+//                 if (sum == target) {
+//                     s.add(Arrays.asList(nums[i], nums[j], nums[k]));
+//                     j++;
+//                     k--;
+//                 } else if (sum < target) {
+//                     j++;
+//                 } else {
+//                     k--;
+//                 }
+//             }
+//         }
+//         output.addAll(s);
+//         return output;
+//     }
+// }
+
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        int target = 0;
+         List<List<Integer>> result = new ArrayList<>();
+        
+        // Sort the array in ascending order
         Arrays.sort(nums);
-        Set<List<Integer>> s = new HashSet<>();
-        List<List<Integer>> output = new ArrayList<>();
-        for (int i = 0; i < nums.length; i++){
-            int j = i + 1;
-            int k = nums.length - 1;
-            while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
-                if (sum == target) {
-                    s.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                    j++;
-                    k--;
-                } else if (sum < target) {
-                    j++;
+        
+        for (int i = 0; i < nums.length - 2; i++) {
+            // Skip duplicate elements for the first element of the triplet
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            
+            int left = i + 1;
+            int right = nums.length - 1;
+            
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                
+                if (sum == 0) {
+                    // Found a valid triplet
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    
+                    // Skip duplicate elements for the second and third elements of the triplet
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+                    
+                    // Move both pointers inward
+                    left++;
+                    right--;
+                } else if (sum < 0) {
+                    // Sum is too small, move left pointer to the right
+                    left++;
                 } else {
-                    k--;
+                    // Sum is too large, move right pointer to the left
+                    right--;
                 }
             }
         }
-        output.addAll(s);
-        return output;
-    }
+        
+        return result;
+    }    
 }
