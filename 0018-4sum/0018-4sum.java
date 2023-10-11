ba4778abd22775b96eaf884d48afdc43560c22fc
Jay <124047536+jaydoshi01@users.noreply.github.com>
2023-10-11 21:05:39 +0530

    Time: 136 ms (16.17%), Space: 44.6 MB (15.99%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -3,26 +3,28 @@
         
 //            List<List<Integer>> result = new ArrayList<>();
         
-//         if (nums == null || nums.length < 4)
-//             return result;
+//      
         
 //         Arrays.sort(nums);
+
         
 //         int n = nums.length;
         
 //         for (int i = 0; i < n - 3; i++) {
-//             if (i > 0 && nums[i] == nums[i - 1])
+//             if (i > 0 && nums[i] == nums[i - 1]){
 //                 continue;
-            
+
+//             }
 //             for (int j = i + 1; j < n - 2; j++) {
-//                 if (j > i + 1 && nums[j] == nums[j - 1])
+//                 if (j > i + 1 && nums[j] == nums[j - 1]){
 //                     continue;
+//                 }
                 
 //                 int left = j + 1;
 //                 int right = n - 1;
                 
 //                 while (left < right) {
-//                     long sum = nums[i] + nums[j] + nums[left] + nums[right];
+//                   long sum = nums[i] + nums[j] + nums[left] + nums[right];
                     
 //                     if (sum == target) {
 //                         result.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
@@ -47,62 +49,93 @@
 //     }
 // }
 
+// class Solution {
+//     public List<List<Integer>> fourSum(int[] nums, int target) {
+//         Arrays.sort(nums);
+//         return kSum(nums, target, 0, 4);
+//     }
+	
+//     public List<List<Integer>> kSum(int[] nums, long target, int start, int k) {
+//         List<List<Integer>> res = new ArrayList<>();
+
+//         // If we have run out of numbers to add, return res.
+//         if (start == nums.length) {
+//             return res;
+//         }
+        
+//         // There are k remaining values to add to the sum. The 
+//         // average of these values is at least target / k.
+//         long average_value = target / k;
+        
+//         // We cannot obtain a sum of target if the smallest value
+//         // in nums is greater than target / k or if the largest 
+//         // value in nums is smaller than target / k.
+//         if  (nums[start] > average_value || average_value > nums[nums.length - 1]) {
+//             return res;
+//         }
+        
+//         if (k == 2) {
+//             return twoSum(nums, target, start);
+//         }
+    
+//         for (int i = start; i < nums.length; ++i) {
+//             if (i == start || nums[i - 1] != nums[i]) {
+//                 for (List<Integer> subset : kSum(nums, target - nums[i], i + 1, k - 1)) {
+//                     res.add(new ArrayList<>(Arrays.asList(nums[i])));
+//                     res.get(res.size() - 1).addAll(subset);
+//                 }
+//             }
+//         }
+    
+//         return res;
+//     }
+	
+//     public List<List<Integer>> twoSum(int[] nums, long target, int start) {
+//         List<List<Integer>> res = new ArrayList<>();
+//         int lo = start, hi = nums.length - 1;
+    
+//         while (lo < hi) {
+//             int currSum = nums[lo] + nums[hi];
+//             if (currSum < target || (lo > start && nums[lo] == nums[lo - 1])) {
+//                 ++lo;
+//             } else if (currSum > target || (hi < nums.length - 1 && nums[hi] == nums[hi + 1])) {
+//                 --hi;
+//             } else {
+//                 res.add(Arrays.asList(nums[lo++], nums[hi--]));
+//             }
+//         }
+                                                          
+//         return res;
+//     }
+// }
+
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         Arrays.sort(nums);
-        return kSum(nums, target, 0, 4);
-    }
-	
-    public List<List<Integer>> kSum(int[] nums, long target, int start, int k) {
-        List<List<Integer>> res = new ArrayList<>();
-
-        // If we have run out of numbers to add, return res.
-        if (start == nums.length) {
-            return res;
-        }
-        
-        // There are k remaining values to add to the sum. The 
-        // average of these values is at least target / k.
-        long average_value = target / k;
-        
-        // We cannot obtain a sum of target if the smallest value
-        // in nums is greater than target / k or if the largest 
-        // value in nums is smaller than target / k.
-        if  (nums[start] > average_value || average_value > nums[nums.length - 1]) {
-            return res;
-        }
-        
-        if (k == 2) {
-            return twoSum(nums, target, start);
-        }
-    
-        for (int i = start; i < nums.length; ++i) {
-            if (i == start || nums[i - 1] != nums[i]) {
-                for (List<Integer> subset : kSum(nums, target - nums[i], i + 1, k - 1)) {
-                    res.add(new ArrayList<>(Arrays.asList(nums[i])));
-                    res.get(res.size() - 1).addAll(subset);
+        Set<List<Integer>> s = new HashSet<>();
+        List<List<Integer>> output = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                int k = j + 1;
+                int l = nums.length - 1;
+                while (k < l) {
+                    long sum = nums[i];
+                    sum += nums[j];
+                    sum += nums[k];
+                    sum += nums[l];
+                    if (sum == target) {
+                        s.add(Arrays.asList(nums[i], nums[j], nums[k], nums[l]));
+                        k++;
+                        l--;
+                    } else if (sum < target) {
+                        k++;
+                    } else {
+                        l--;
+                    }
                 }
             }
         }
-    
-        return res;
-    }
-	
-    public List<List<Integer>> twoSum(int[] nums, long target, int start) {
-        List<List<Integer>> res = new ArrayList<>();
-        int lo = start, hi = nums.length - 1;
-    
-        while (lo < hi) {
-            int currSum = nums[lo] + nums[hi];
-            if (currSum < target || (lo > start && nums[lo] == nums[lo - 1])) {
-                ++lo;
-            } else if (currSum > target || (hi < nums.length - 1 && nums[hi] == nums[hi + 1])) {
-                --hi;
-            } else {
-                res.add(Arrays.asList(nums[lo++], nums[hi--]));
-            }
-        }
-                                                          
-        return res;
+        output.addAll(s);
+        return output;
     }
 }
