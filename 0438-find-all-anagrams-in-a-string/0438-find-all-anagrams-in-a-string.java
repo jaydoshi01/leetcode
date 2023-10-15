3b096d80ec487a04be42181b305d360a2343a4e8
Jay <124047536+jaydoshi01@users.noreply.github.com>
2023-10-15 14:25:22 +0530

    Time: 8 ms (93.75%), Space: 43.8 MB (65.64%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,34 +1,70 @@
+// class Solution {
+//   public List < Integer > findAnagrams(String s, String p) {
+//     int freq1[] = new int[26];
+//     int freq2[] = new int[26];
+//     List < Integer > list = new ArrayList < > ();
+
+//     if (s.length() < p.length())
+//       return list;
+
+//     for (int i = 0; i < p.length(); i++) {
+//       freq1[s.charAt(i) - 'a']++;
+//       freq2[p.charAt(i) - 'a']++;
+//     }
+
+//     int start = 0;
+//     int end = p.length();
+
+//     if (Arrays.equals(freq1, freq2))
+//       list.add(start);
+
+//     while (end < s.length()) {
+
+//       freq1[s.charAt(start) - 'a']--;
+//       freq1[s.charAt(end) - 'a']++;
+
+//       if (Arrays.equals(freq1, freq2))
+//         list.add(start + 1);
+
+//       start++;
+//       end++;
+//     }
+//     return list;
+//   }
+// }
+
+
 class Solution {
-  public List < Integer > findAnagrams(String s, String p) {
-    int freq1[] = new int[26];
-    int freq2[] = new int[26];
-    List < Integer > list = new ArrayList < > ();
-
-    if (s.length() < p.length())
-      return list;
-
-    for (int i = 0; i < p.length(); i++) {
-      freq1[s.charAt(i) - 'a']++;
-      freq2[p.charAt(i) - 'a']++;
+    public List<Integer> findAnagrams(String s1, String s2) {
+        int s = s1.length(), p = s2.length();
+        if(s < p)
+            return new ArrayList<>();
+        
+        List<Integer> li = new ArrayList<>();
+        int[] c1 = new int[26];
+        int[] c2 = new int[26];
+        
+        for(int i=0; i<p; i++) {
+            c2[s2.charAt(i) - 'a']++;
+            c1[s1.charAt(i) - 'a']++;
+        }
+        if(areEqual(c1, c2))
+            li.add(0);
+        
+        for(int i=p; i<s; i++) {
+            c1[s1.charAt(i-p) - 'a']--;
+            c1[s1.charAt(i) - 'a']++;
+            if(areEqual(c1, c2))
+                li.add(i-p+1);
+        }
+        
+        return li;
     }
-
-    int start = 0;
-    int end = p.length();
-
-    if (Arrays.equals(freq1, freq2))
-      list.add(start);
-
-    while (end < s.length()) {
-
-      freq1[s.charAt(start) - 'a']--;
-      freq1[s.charAt(end) - 'a']++;
-
-      if (Arrays.equals(freq1, freq2))
-        list.add(start + 1);
-
-      start++;
-      end++;
+    
+    private boolean areEqual(int[] a, int[] b) {
+        for(int i=0; i<26; i++)
+            if(a[i] != b[i])
+                return false;
+        return true;
     }
-    return list;
-  }
 }
