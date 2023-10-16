25ee132007e34f7dae545c940d74ee6bbffc5575
Jay <124047536+jaydoshi01@users.noreply.github.com>
2023-10-16 12:48:39 +0530

    Time: 9 ms (88.18%), Space: 43.6 MB (88.38%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,37 +1,37 @@
-// class Solution {
-//   public List < Integer > findAnagrams(String s, String p) {
-//     int freq1[] = new int[26];
-//     int freq2[] = new int[26];
-//     List < Integer > list = new ArrayList < > ();
+class Solution {
+  public List < Integer > findAnagrams(String s, String p) {
+    int freq1[] = new int[26];
+    int freq2[] = new int[26];
+    List < Integer > list = new ArrayList < > ();
 
-//     if (s.length() < p.length())
-//       return list;
+    if (s.length() < p.length())
+      return list;
 
-//     for (int i = 0; i < p.length(); i++) {
-//       freq1[s.charAt(i) - 'a']++;
-//       freq2[p.charAt(i) - 'a']++;
-//     }
+    for (int i = 0; i < p.length(); i++) {
+      freq1[s.charAt(i) - 'a']++;
+      freq2[p.charAt(i) - 'a']++;
+    }
 
-//     int start = 0;
-//     int end = p.length();
+    int start = 0;
+    int end = p.length();
 
-//     if (Arrays.equals(freq1, freq2))
-//       list.add(start);
+    if (Arrays.equals(freq1, freq2))
+      list.add(start);
 
-//     while (end < s.length()) {
+    while (end < s.length()) {
 
-//       freq1[s.charAt(start) - 'a']--;
-//       freq1[s.charAt(end) - 'a']++;
+      freq1[s.charAt(start) - 'a']--;
+      freq1[s.charAt(end) - 'a']++;
 
-//       if (Arrays.equals(freq1, freq2))
-//         list.add(start + 1);
+      if (Arrays.equals(freq1, freq2))
+        list.add(start + 1);
 
-//       start++;
-//       end++;
-//     }
-//     return list;
-//   }
-// }
+      start++;
+      end++;
+    }
+    return list;
+  }
+}
 
 
 // class Solution {
@@ -69,45 +69,3 @@
 //     }
 // }
 
-
-class Solution {
-    public List<Integer> findAnagrams(String s, String p) {
-        List<Integer> res = new ArrayList<>();// our result
-        int i=0,j=0; // for tracking window
-        int arr[] = new int[26]; // for tracking count of alphabets
-        int eleCount = 0; // for tracking if the alphabet array contains all zeroes (found anagram)
-        int k = p.length();
-
-        // populating array with alphabet count in p
-        for(int x=0;x<k;x++){
-            if(arr[p.charAt(x)-97]==0)eleCount++;
-            arr[p.charAt(x)-97]++;
-        }
-
-        // sliding window approach
-        while(j<s.length()){
-            // if count of alphabet is 0 before an operation then it will increase or decrease
-            if(arr[s.charAt(j)-97]==0)eleCount++;
-            arr[s.charAt(j)-97]--;// decreasing count as we encounter alphabet (operation)
-            if(arr[s.charAt(j)-97]==0)eleCount--;
-            
-            if((j-i+1)<k){// hitting the window size
-                j++;
-            }
-            else{
-                // if all zero then put the starting index
-                if(eleCount==0){
-                    res.add(i);
-                }
-                
-                if(arr[s.charAt(i)-97]==0)eleCount++;
-                arr[s.charAt(i)-97]++; // increasing because we are leaving that alphabet
-                if(arr[s.charAt(i)-97]==0)eleCount--;
-               // sliding the window
-                i++;
-                j++;
-            }
-        }
-        return res;
-    }
-}
