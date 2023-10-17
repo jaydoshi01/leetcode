e33d82ecbba9a4994c5c3ee6058a72cf107ab92c
Jay <124047536+jaydoshi01@users.noreply.github.com>
2023-10-17 19:33:59 +0530

    Time: 0 ms (100%), Space: 55.9 MB (45.93%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,18 +1,46 @@
 class Solution {
-    public void rotate(int[] nums, int k) {
-        k %= nums.length;
-        int n = nums.length;
-        reverseNum(nums,0,n-1);
-        reverseNum(nums,0,k-1);
-        reverseNum(nums,k,n-1);
-    }
-    public void reverseNum(int[] nums, int start, int end) {
-        while(start < end) {
-            int temp = nums[start];
-            nums[start] = nums[end];
-            nums[end] = temp;
-            start++;
-            end--;
-        }
+
+    public static void reverse(int nums[], int start, int end){
+    // While start index is less than end index
+    while(start < end){
+        // Swap elements at start and end indices
+        int temp = nums[start];
+        nums[start] = nums[end];
+        nums[end] = temp;
+        // Increment start index and decrement end index
+        start++;
+        end--;
     }
 }
+
+    public void rotate(int[] nums, int k) {
+        // Ensure k is within array bounds
+        k %= nums.length;
+        // Reverse entire array
+        reverse(nums, 0, nums.length - 1);
+        // Reverse first k elements
+        reverse(nums, 0, k - 1);
+        // Reverse remaining elements
+        reverse(nums, k, nums.length - 1);
+    }
+}
+
+
+// class Solution {
+//     public void rotate(int[] nums, int k) {
+//         k %= nums.length;
+//         int n = nums.length;
+//         reverseNum(nums,0,n-1);
+//         reverseNum(nums,0,k-1);
+//         reverseNum(nums,k,n-1);
+//     }
+//     public void reverseNum(int[] nums, int start, int end) {
+//         while(start < end) {
+//             int temp = nums[start];
+//             nums[start] = nums[end];
+//             nums[end] = temp;
+//             start++;
+//             end--;
+//         }
+//     }
+// }
