62fc43398203cb896f237396d82c9375a80ec3ae
Jay <124047536+jaydoshi01@users.noreply.github.com>
2023-10-24 07:48:08 +0530

    Time: 208 ms (20.04%), Space: 45.2 MB (55.61%) - LeetHub

---
+++ 0924-fair-candy-swap/0924-fair-candy-swap.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] fairCandySwap(int[] aliceSizes, int[] bobSizes) {
+        
+        //using two pinter approach.
+        int aliceTotal = 0;
+        int bobTotal = 0;
+        //Find sum of alice candies before exchange.
+        for(int aa : aliceSizes)
+            aliceTotal += aa;
+        //Find sum of bob candies before exchange.
+        for(int bb : bobSizes)
+            bobTotal += bb;
+        
+        for(int i = 0; i< aliceSizes.length; i++){
+            for(int j = 0; j< bobSizes.length; j++){
+                int aliceExchange = aliceTotal - aliceSizes[i] + bobSizes[j];
+                int bobExchange = bobTotal - bobSizes[j] + aliceSizes[i];
+                if(aliceExchange == bobExchange){
+                    return new int[]{aliceSizes[i], bobSizes[j]};
+                }
+            }
+        }
+        return new int[0];
+    }
+}
