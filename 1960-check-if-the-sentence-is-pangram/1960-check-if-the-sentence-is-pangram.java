e1a3db586e76e67c31285ccfc73af418542b2da5
Jay <124047536+jaydoshi01@users.noreply.github.com>
2023-11-05 14:44:05 +0530

    Time: 1 ms (85.81%), Space: 40.3 MB (77.97%) - LeetHub

--- 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.java
+++ 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.java
@@ -1,23 +1,39 @@
 class Solution {
     public boolean checkIfPangram(String sentence) {
-        //declare a array to maintain every char occurance
-        int a[]=new int[26];
-        //traverse String
-        for(int i=0;i<sentence.length();i++)
-        {
-            int ch;
-            ch=sentence.charAt(i);
-            if(a[ch-97]==1)
-                continue;
-            else
-                a[ch-97]=1;
+    //     //declare a array to maintain every char occurance
+    //     int a[]=new int[26];
+    //     //traverse String
+    //     for(int i=0;i<sentence.length();i++)
+    //     {
+    //         int ch;
+    //         ch=sentence.charAt(i);
+    //         if(a[ch-97]==1)
+    //             continue;
+    //         else
+    //             a[ch-97]=1;
+    //     }
+    //     //traverse the Array to check for every char occurance 
+    //     for(int i=0;i<a.length;i++)
+    //     {
+    //         if(a[i]!=1)
+    //             return false;
+    //     }
+    //     return true;
+    // }
+
+
+        int[] arr = new int[26];
+        int count = 0;
+    
+    for (int i = 0; i < sentence.length(); i++) {
+        if (arr[sentence.charAt(i) - 'a'] == 0) {
+            arr[sentence.charAt(i) - 'a'] += 1;
+            count++;
         }
-        //traverse the Array to check for every char occurance 
-        for(int i=0;i<a.length;i++)
-        {
-            if(a[i]!=1)
-                return false;
+        if (count == 26) {
+            return true;
         }
-        return true;
     }
+    return false;
+}
 }
