beba96deef612c213cc35c11145b11cf0e0da81d
Jay <124047536+jaydoshi01@users.noreply.github.com>
2023-12-07 04:42:47 +0530

    Time: 2 ms (76.11%), Space: 40.8 MB (25.72%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -1,9 +1,29 @@
+// class Solution {
+// public boolean checkIfPangram(String sentence) {
+//     int cnt[] = new int[26], total = 0;
+//     for (int ch: sentence.toCharArray())
+//         if (++cnt[ch - 'a'] == 1)
+//             total++;
+//     return total == 26;
+// }
+// }
+    
+
 class Solution {
-public boolean checkIfPangram(String sentence) {
-    int cnt[] = new int[26], total = 0;
-    for (var ch: sentence.toCharArray())
-        if (++cnt[ch - 'a'] == 1)
-            total++;
-    return total == 26;
+    public boolean checkIfPangram(String sentence) {
+        int[] letters = new int[26];
+        for(int i = 0 ;i< 26;i++){
+            letters[i] = -1;
+        }
+        for(int i = 0;i<sentence.length();i++){
+            char ch = sentence.charAt(i);
+            // System.out.println(ch);
+            letters[ch - 'a']++;
+        }
+        for(int i : letters){
+            if(i == -1)return false;
+        }
+        return true;
+    }
 }
-}
+   
