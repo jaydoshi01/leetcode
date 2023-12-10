258e7cae435e3a29021ba086096dfaf07af87a02
Jay <124047536+jaydoshi01@users.noreply.github.com>
2023-12-10 20:10:44 +0530

    Time: 0 ms (100.00%), Space: 41 MB (23.52%) - LeetHub

--- 1854-maximum-population-year/1854-maximum-population-year.java
+++ 1854-maximum-population-year/1854-maximum-population-year.java
@@ -1,19 +1,45 @@
+// class Solution {
+//     public int maximumPopulation(int[][] logs) {
+//      int[] map = new int[101]; // Based on problem statement: 1950 <= birthi < deathi <= 2050, so we only need 101.
+//         for(int[] l : logs){
+//             for(int i = l[0]; i < l[1]; i++){
+//                 map[i - 1950]++;
+//             }
+//         }
+//         int max = 0;
+//         int res = -1;
+//         for(int i = 0; i < 101; i++){
+//             if(map[i] > max){
+//                 max = map[i];
+//                 res = i + 1950;
+//             }
+//         }
+//         return res; 
+// }
+// }
+
 class Solution {
     public int maximumPopulation(int[][] logs) {
-     int[] map = new int[101]; // Based on problem statement: 1950 <= birthi < deathi <= 2050, so we only need 101.
-        for(int[] l : logs){
-            for(int i = l[0]; i < l[1]; i++){
-                map[i - 1950]++;
+        
+        int[] year = new int[2051];
+        
+        for(int[] log : logs){
+            
+            year[log[0]] += 1;
+            year[log[1]] -= 1;
+        }
+        
+        int maxNum = year[1950], maxYear = 1950;
+        
+        for(int i = 1951; i < year.length; i++){
+            year[i] += year[i - 1];  
+            
+            if(year[i] > maxNum){
+                maxNum = year[i];
+                maxYear = i;
             }
         }
-        int max = 0;
-        int res = -1;
-        for(int i = 0; i < 101; i++){
-            if(map[i] > max){
-                max = map[i];
-                res = i + 1950;
-            }
-        }
-        return res; 
-}
+        
+        return maxYear;
+    }
 }
