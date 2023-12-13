3df0fff3be74c2dd4c00b3ddacb60ddf28ccb441
Jay <124047536+jaydoshi01@users.noreply.github.com>
2023-12-13 10:14:39 +0530

    Time: 4 ms (79.60%), Space: 44.6 MB (13.43%) - LeetHub

--- 0885-spiral-matrix-iii/0885-spiral-matrix-iii.java
+++ 0885-spiral-matrix-iii/0885-spiral-matrix-iii.java
@@ -1,37 +1,53 @@
 class Solution {
     public int[][] spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
-        int[][] result = new int[rows * cols][2];
-        int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}}; // Right, Down, Left, Up
+        int[][] matrix = new int[rows * cols][2];
+        int k = 0;
 
-        int index = 0;
-        int steps = 1;
+        int rowEnd = rStart + 1;
+        int colEnd = cStart + 1;
 
-        result[index++] = new int[]{rStart, cStart};
-
-        while (index < rows * cols) {
-            for (int i = 0; i < 4; i++) {
-                int[] direction = directions[i];
-
-                for (int j = 0; j < steps; j++) {
-                    rStart += direction[0];
-                    cStart += direction[1];
-
-                    if (isValidPosition(rStart, cStart, rows, cols)) {
-                        result[index++] = new int[]{rStart, cStart};
-                        
-                        if (index == rows * cols) {
-                            return result;
-                        }
-                    }
-                }
-
-                if (i % 2 == 1) {
-                    steps++; // Increase steps after moving in both horizontal and vertical directions
+        while (true) {
+            // Right
+            for (int i = cStart; i <= colEnd; i++) {
+                if (isValidPosition(rStart, i, rows, cols)) {
+                    matrix[k++] = new int[]{rStart, i};
                 }
             }
-        }
+            cStart--;
+            colEnd++;
 
-        return result;
+            if (k == matrix.length) break;
+
+            // Down
+            for (int i = rStart + 1; i <= rowEnd; i++) {
+                if (isValidPosition(i, colEnd - 1, rows, cols)) {
+                    matrix[k++] = new int[]{i, colEnd - 1};
+                }
+            }
+            rStart--;
+            rowEnd++;
+
+            if (k == matrix.length) break;
+
+            // Left
+            for (int i = colEnd - 2; i >= cStart; i--) {
+                if (isValidPosition(rowEnd - 1, i, rows, cols)) {
+                    matrix[k++] = new int[]{rowEnd - 1, i};
+                }
+            }
+
+            if (k == matrix.length) break;
+
+            // Up
+            for (int i = rowEnd - 2; i > rStart; i--) {
+                if (isValidPosition(i, cStart, rows, cols)) {
+                    matrix[k++] = new int[]{i, cStart};
+                }
+            }
+
+            if (k == matrix.length) break;
+        }
+        return matrix;
     }
 
     private boolean isValidPosition(int row, int col, int rows, int cols) {
