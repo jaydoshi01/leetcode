743b551eb8d07719ae5d4515a093a9a810c8a7b4
Jay <124047536+jaydoshi01@users.noreply.github.com>
2023-12-19 14:06:09 +0530

    Time: 0 ms (100.00%), Space: 56.1 MB (35.85%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,46 +1,20 @@
 class Solution {
-
-    public static void reverse(int nums[], int start, int end){
-    // While start index is less than end index
-    while(start < end){
-        // Swap elements at start and end indices
-        int temp = nums[start];
-        nums[start] = nums[end];
-        nums[end] = temp;
-        // Increment start index and decrement end index
-        start++;
-        end--;
-    }
-}
-
     public void rotate(int[] nums, int k) {
-        // Ensure k is within array bounds
-        k %= nums.length;
-        // Reverse entire array
-        reverse(nums, 0, nums.length - 1);
-        // Reverse first k elements
-        reverse(nums, 0, k - 1);
-        // Reverse remaining elements
-        reverse(nums, k, nums.length - 1);
+       int n=nums.length;
+        k=k%n;
+        flip(nums ,0 , n-1);
+        flip(nums ,0 , k-1);
+        flip(nums ,k , n-1);
+        
     }
+    
+    public void flip(int[] arr , int s , int e){
+        while(s < e){
+        int temp = arr[s];
+        arr[s]=arr[e];
+        arr[e]=temp;
+        s++;
+        e--;
+     }
+   }
 }
-
-
-// class Solution {
-//     public void rotate(int[] nums, int k) {
-//         k %= nums.length;
-//         int n = nums.length;
-//         reverseNum(nums,0,n-1);
-//         reverseNum(nums,0,k-1);
-//         reverseNum(nums,k,n-1);
-//     }
-//     public void reverseNum(int[] nums, int start, int end) {
-//         while(start < end) {
-//             int temp = nums[start];
-//             nums[start] = nums[end];
-//             nums[end] = temp;
-//             start++;
-//             end--;
-//         }
-//     }
-// }
