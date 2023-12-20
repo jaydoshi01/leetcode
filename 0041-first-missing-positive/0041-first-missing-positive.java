5d07b0914bae9f1333b551f91d6c0f049e85a44e
Jay <124047536+jaydoshi01@users.noreply.github.com>
2023-12-20 18:45:00 +0530

    Time: 1 ms (99.15%), Space: 55 MB (97.34%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,36 +1,83 @@
-public class Solution {
-public int firstMissingPositive(int[] nums) {
-    int n = nums.length;
+// public class Solution {
+// public int firstMissingPositive(int[] nums) {
+//     int n = nums.length;
     
-    // 1. mark numbers (num < 0) and (num > n) with a special marker number (n+1) 
-    // (we can ignore those because if all number are > n then we'll simply return 1)
-    for (int i = 0; i < n; i++) {
-        if (nums[i] <= 0 || nums[i] > n) {
-            nums[i] = n + 1;
+//     // 1. mark numbers (num < 0) and (num > n) with a special marker number (n+1) 
+//     // (we can ignore those because if all number are > n then we'll simply return 1)
+//     for (int i = 0; i < n; i++) {
+//         if (nums[i] <= 0 || nums[i] > n) {
+//             nums[i] = n + 1;
+//         }
+//     }
+//     // note: all number in the array are now positive, and on the range 1..n+1
+    
+//     // 2. mark each cell appearing in the array, by converting the index for that number to negative
+//     for (int i = 0; i < n; i++) {
+//         int num = Math.abs(nums[i]);
+//         if (num > n) {
+//             continue;
+//         }
+//         num--; // -1 for zero index based array (so the number 1 will be at pos 0)
+//         if (nums[num] > 0) { // prevents double negative operations
+//             nums[num] = -1 * nums[num];
+//         }
+//     }
+    
+//     // 3. find the first cell which isn't negative (doesn't appear in the array)
+//     for (int i = 0; i < n; i++) {
+//         if (nums[i] >= 0) {
+//             return i + 1;
+//         }
+//     }
+    
+//     // 4. no positive numbers were found, which means the array contains all numbers 1..n
+//     return n + 1;
+// }
+// }
+
+
+// public class Solution {
+// public int firstMissingPositive(int[] nums) {
+    
+//    }
+// } 
+
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+
+        // Rearrange the array such that nums[i] is equal to i+1
+        for (int i = 0; i < n; i++) {
+            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+                int temp = nums[nums[i] - 1];
+                nums[nums[i] - 1] = nums[i];
+                nums[i] = temp;
+            }
         }
+
+        // Find the first index i where nums[i] != i+1
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+
+        // If all elements are in their correct positions, return n+1
+        return n + 1;
     }
-    // note: all number in the array are now positive, and on the range 1..n+1
-    
-    // 2. mark each cell appearing in the array, by converting the index for that number to negative
-    for (int i = 0; i < n; i++) {
-        int num = Math.abs(nums[i]);
-        if (num > n) {
-            continue;
-        }
-        num--; // -1 for zero index based array (so the number 1 will be at pos 0)
-        if (nums[num] > 0) { // prevents double negative operations
-            nums[num] = -1 * nums[num];
-        }
-    }
-    
-    // 3. find the first cell which isn't negative (doesn't appear in the array)
-    for (int i = 0; i < n; i++) {
-        if (nums[i] >= 0) {
-            return i + 1;
-        }
-    }
-    
-    // 4. no positive numbers were found, which means the array contains all numbers 1..n
-    return n + 1;
 }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
