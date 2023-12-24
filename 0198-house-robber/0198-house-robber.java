e4a1324689765446497f66f324fc5045ffd799ae
Jay <124047536+jaydoshi01@users.noreply.github.com>
2023-12-24 14:41:04 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (5.10%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,26 +1,27 @@
 class Solution {
-    // public int rob(int[] nums) {
-    //     int pre = 0, cur = 0;
-    //     for (int num : nums) {
-    //         final int temp = Integer.max(pre + num, cur);
-    //         pre = cur;
-    //         cur = temp;
-    //     }
-    //     return cur;
-    // }
-    
-    
-    
-    
-   public int rob(int[] nums) {
-    if (nums.length < 2) return nums[0];
-    int[] memo = new int[nums.length];
-    memo[0] = nums[0];
-    memo[1] = Math.max(nums[0] , nums[1]);
-    for (int i = 2; i < nums.length; i++) {
-        
-        memo[i] = Math.max(memo[i-2]+nums[i], memo[i-1]);
+    public int rob(int[] nums) {
+        int pre = 0, cur = 0;
+        for (int num : nums) {
+            final int temp = Integer.max(pre + num, cur);
+            pre = cur;
+            cur = temp;
+        }
+        return cur;
     }
-    return memo[nums.length-1];
 }
-}
+    
+    
+    
+    
+//    public int rob(int[] nums) {
+//     if (nums.length < 2) return nums[0];
+//     int[] memo = new int[nums.length];
+//     memo[0] = nums[0];
+//     memo[1] = Math.max(nums[0] , nums[1]);
+//     for (int i = 2; i < nums.length; i++) {
+        
+//         memo[i] = Math.max(memo[i-2]+nums[i], memo[i-1]);
+//     }
+//     return memo[nums.length-1];
+// }
+// }
