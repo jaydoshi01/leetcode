b81e1f2685cb2b95683406eb893bf6c4c2469e49
Jay <124047536+jaydoshi01@users.noreply.github.com>
2023-12-24 14:38:55 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (5.10%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -8,15 +8,19 @@ class Solution {
     //     }
     //     return cur;
     // }
+    
+    
+    
+    
    public int rob(int[] nums) {
-    if (nums.length == 0) return 0;
-    int[] memo = new int[nums.length + 1];
-    memo[0] = 0;
-    memo[1] = nums[0];
-    for (int i = 1; i < nums.length; i++) {
-        int val = nums[i];
-        memo[i+1] = Math.max(memo[i], memo[i-1] + val);
+    if (nums.length < 2) return nums[0];
+    int[] memo = new int[nums.length];
+    memo[0] = nums[0];
+    memo[1] = Math.max(nums[0] , nums[1]);
+    for (int i = 2; i < nums.length; i++) {
+        
+        memo[i] = Math.max(memo[i-2]+nums[i], memo[i-1]);
     }
-    return memo[nums.length];
+    return memo[nums.length-1];
 }
 }
