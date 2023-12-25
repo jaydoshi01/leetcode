e93459380899d9cb5a96188a1f2f05aceae658fd
Jay <124047536+jaydoshi01@users.noreply.github.com>
2023-12-25 13:02:02 +0530

    Time: 2 ms (84.86%), Space: 47.1 MB (5.25%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,44 +1,44 @@
-class Solution {
-     public int[] twoSum(int[] numbers, int target) {
-        for (int i = 0; i < numbers.length; ++i) {
-            int low = i + 1;
-            int high = numbers.length - 1;
-            while (low <= high) {
-                int mid = (high - low) / 2 + low;
-                if (numbers[mid] == target - numbers[i]) {
-                    return new int[]{i + 1, mid + 1};
-                } else if (numbers[mid] > target - numbers[i]) {
-                    high = mid - 1;
-                } else {
-                    low = mid + 1;
-                }
-            }
-        }
-        return new int[]{-1, -1};
-    }
-}
-
 // class Solution {
-//     public int[] twoSum(int[] num, int target) {
-//          int[] indice = new int[2];
-//     if (num == null || num.length < 2) return indice;
-//     int left = 0, right = num.length - 1;
-//     while (left < right) {
-//         int v = num[left] + num[right];
-//         if (v == target) {
-//             indice[0] = left + 1;
-//             indice[1] = right + 1;
-//             break;
-//         } else if (v > target) {
-//             right --;
-//         } else {
-//             left ++;
+//      public int[] twoSum(int[] numbers, int target) {
+//         for (int i = 0; i < numbers.length; ++i) {
+//             int low = i + 1;
+//             int high = numbers.length - 1;
+//             while (low <= high) {
+//                 int mid = (high - low) / 2 + low;
+//                 if (numbers[mid] == target - numbers[i]) {
+//                     return new int[]{i + 1, mid + 1};
+//                 } else if (numbers[mid] > target - numbers[i]) {
+//                     high = mid - 1;
+//                 } else {
+//                     low = mid + 1;
+//                 }
+//             }
 //         }
-//     }
-//     return indice;
-        
+//         return new int[]{-1, -1};
 //     }
 // }
+
+class Solution {
+    public int[] twoSum(int[] num, int target) {
+         int[] indice = new int[2];
+    if (num == null || num.length < 2) return indice;
+    int left = 0, right = num.length - 1;
+    while (left < right) {
+        int v = num[left] + num[right];
+        if (v == target) {
+            indice[0] = left + 1;
+            indice[1] = right + 1;
+            break;
+        } else if (v > target) {
+            right --;
+        } else {
+            left ++;
+        }
+    }
+    return indice;
+        
+    }
+}
     
     
     // public int[] twoSum(int[] numbers, int target) {
