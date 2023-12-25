c15838715e95d0ef2b19ffc0b78a9f9259397052
Jay <124047536+jaydoshi01@users.noreply.github.com>
2023-12-25 13:05:03 +0530

    Time: 0 ms (100.00%), Space: 39.9 MB (14.28%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.java
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -1,26 +1,25 @@
-// class Solution {
-//     public boolean isPerfectSquare(int num) {
-//         int low = 1, high = num;
-//         while (low <= high) {
-//             long mid = (low + high) >>> 1;
-//             if (mid * mid == num) {
-//                 return true;
-//             } else if (mid * mid < num) {
-//                 low = (int) mid + 1;
-//             } else {
-//                 high = (int) mid - 1;
-//             }
-//         }
-//         return false;
-//     }
-// }
-
 class Solution {
     public boolean isPerfectSquare(int num) {
-        double a = Math.sqrt(num);
-        if(Math.ceil(a)==Math.floor(a)){
-            return true;
+        long left = 1, right = num;
+        while (left <= right) {
+            long mid = (left + right) / 2;
+            if (mid * mid == num) return true; // check if mid is perfect square
+            if (mid * mid < num) { // mid is small -> go right to increase mid
+                left = mid + 1;
+            } else {
+                right = mid - 1; // mid is large -> to left to decrease mid
+            }
         }
         return false;
     }
 }
+
+// class Solution {
+//     public boolean isPerfectSquare(int num) {
+//         double a = Math.sqrt(num);
+//         if(Math.ceil(a)==Math.floor(a)){
+//             return true;
+//         }
+//         return false;
+//     }
+// }
