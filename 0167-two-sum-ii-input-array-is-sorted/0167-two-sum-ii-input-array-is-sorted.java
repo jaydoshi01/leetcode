51bafb1de98eac84757202d9edb179a4c3c4ede6
Jay <124047536+jaydoshi01@users.noreply.github.com>
2023-12-25 12:49:46 +0530

    Time: 2 ms (84.86%), Space: 47.2 MB (5.25%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,21 +1,33 @@
 class Solution {
-    public int[] twoSum(int[] num, int target) {
-         int[] indice = new int[2];
-    if (num == null || num.length < 2) return indice;
-    int left = 0, right = num.length - 1;
-    while (left < right) {
-        int v = num[left] + num[right];
-        if (v == target) {
-            indice[0] = left + 1;
-            indice[1] = right + 1;
-            break;
-        } else if (v > target) {
-            right --;
-        } else {
-            left ++;
-        }
-    }
-    return indice;
+//     public int[] twoSum(int[] num, int target) {
+//          int[] indice = new int[2];
+//     if (num == null || num.length < 2) return indice;
+//     int left = 0, right = num.length - 1;
+//     while (left < right) {
+//         int v = num[left] + num[right];
+//         if (v == target) {
+//             indice[0] = left + 1;
+//             indice[1] = right + 1;
+//             break;
+//         } else if (v > target) {
+//             right --;
+//         } else {
+//             left ++;
+//         }
+//     }
+//     return indice;
         
+//     }
+    
+    
+    
+    public int[] twoSum(int[] numbers, int target) {
+        int start = 0, end = numbers.length - 1;
+        while(start < end){
+            if(numbers[start] + numbers[end] == target) break;
+            if(numbers[start] + numbers[end] < target) start++;
+            else end--;
+        }
+        return new int[]{start + 1, end + 1};
     }
 }
