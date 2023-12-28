0c1b027eb84975ed7e3eb258669ce09d0213b74a
Jay <124047536+jaydoshi01@users.noreply.github.com>
2023-12-28 15:15:57 +0530

    Time: 1 ms (60.62%), Space: 41.5 MB (5.20%) - LeetHub

--- 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.java
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.java
@@ -1,16 +1,37 @@
+// class Solution {
+//     public int specialArray(int[] nums) {
+//         for(int i = 1 ; i < 1001 ; i++){
+//             int count = 0;
+//             for(int num : nums){
+//                 if(num >= i)
+//                     count++;     
+//             }
+//                  if(i==count)
+//                     return i;
+//                 }  
+//                    return -1;     
+//     }
+// }
+
+
 class Solution {
     public int specialArray(int[] nums) {
-        for(int i = 1 ; i < 1001 ; i++){
-            int count = 0;
-            for(int num : nums){
-                if(num >= i)
-                    count++;
-                
+        int[] bucket = new int[1001];
+        for(int num : nums){
+            bucket[num]++;
+        }
+            int total = nums.length;
+        for(int i = 0 ; i < 1001 ; i++){
+            if(i==total){
+                return i;
+            }else{
+                total -= bucket[i];
             }
-                if(i==count)
-                    return i;
-                }
-                
-             return -1;     
+        }
+        return -1;
     }
 }
+
+
+
+
