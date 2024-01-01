4d4aad126668fd88a4744088ed188c77878e70c2
Jay <124047536+jaydoshi01@users.noreply.github.com>
2024-01-01 21:56:19 +0530

    Time: 15 ms (5.45%), Space: 45 MB (5.27%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,20 +1,36 @@
 class Solution {
-  public int romanToInt(String s) {
-    HashMap<String, Integer> table = new HashMap<>();
-    table.put("I",1);
-    table.put("V",5);
-    table.put("X",10);
-    table.put("L",50);
-    table.put("C",100);
-    table.put("D",500);
-    table.put("M",1000);
-    int num = 0;
+//   public int romanToInt(String s) {
+//     HashMap<String, Integer> table = new HashMap<>();
+//     table.put("I",1);
+//     table.put("V",5);
+//     table.put("X",10);
+//     table.put("L",50);
+//     table.put("C",100);
+//     table.put("D",500);
+//     table.put("M",1000);
+//     int num = 0;
      
-    for(int i = s.length() - 1; i >= 0; i--)
-      if (i == s.length() - 1 || table.get(Character.toString(s.charAt(i))) >= table.get(Character.toString(s.charAt(i+1))))
-           num += table.get(Character.toString(s.charAt(i)));
-      else num -= table.get(Character.toString(s.charAt(i)));
+//     for(int i = s.length() - 1; i >= 0; i--)
+//       if (i == s.length() - 1 || table.get(Character.toString(s.charAt(i))) >= table.get(Character.toString(s.charAt(i+1))))
+//            num += table.get(Character.toString(s.charAt(i)));
+//       else num -= table.get(Character.toString(s.charAt(i)));
     
-    return num;
-  }
+//     return num;
+//   }
+    
+    public int romanToInt(String s) {
+        Map<Character, Integer> alphabet = initAlphabet();
+        int result = 0;
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (alphabet.get(s.charAt(i)) < alphabet.get(s.charAt(i + 1)))
+                result = result - alphabet.get(s.charAt(i));
+            else 
+                result = result + alphabet.get(s.charAt(i));
+        }
+        return result + alphabet.get(s.charAt(s.length() - 1));
+    }
+
+    private Map<Character, Integer> initAlphabet() {
+        return Map.of('I', 1, 'V', 5, 'X', 10, 'L', 50, 'C', 100, 'D', 500, 'M', 1000);
+    }
 }
