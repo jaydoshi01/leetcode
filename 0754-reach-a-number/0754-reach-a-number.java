8e37827eee44ff63823ab79b2b24fc523e3e6128
Jay <124047536+jaydoshi01@users.noreply.github.com>
2024-01-05 10:56:07 +0530

    Time: 1 ms (84.67%), Space: 40.1 MB (22.61%) - LeetHub

--- 0754-reach-a-number/0754-reach-a-number.java
+++ 0754-reach-a-number/0754-reach-a-number.java
@@ -1,18 +1,28 @@
+// class Solution {
+//     public int reachNumber(int target) {
+        
+//        target = Math.abs(target);
+//         int step = 0 ; 
+//         int sum = 0 ; 
+//         while( sum < target){
+//             step++;
+//             sum+=step;
+//         }
+//         while((sum - target) % 2 != 0){
+//             step++;
+//             sum+=step;    
+//         }
+//          return step;
+//     }
+   
+// }
+
 class Solution {
     public int reachNumber(int target) {
-        
-       target = Math.abs(target);
-        int step = 0 ; 
-        int sum = 0 ; 
-        while( sum < target){
-            step++;
-            sum+=step;
-        }
-        while((sum - target)%2 != 0){
-            step++;
-            sum+=step;    
-        }
-         return step;
+        target = Math.abs(target);
+        int k = 0;
+        while (target > 0)
+            target -= ++k;
+        return target % 2 == 0 ? k : k + 1 + k%2;
     }
-   
 }
