bcdc9e9fa0ca8b9e56b1f84ed3e257a1257c09aa
Jay <124047536+jaydoshi01@users.noreply.github.com>
2024-01-06 13:04:23 +0530

    Time: 1 ms (94.28%), Space: 40.9 MB (5.72%) - LeetHub

--- 1802-maximum-value-at-a-given-index-in-a-bounded-array/1802-maximum-value-at-a-given-index-in-a-bounded-array.java
+++ 1802-maximum-value-at-a-given-index-in-a-bounded-array/1802-maximum-value-at-a-given-index-in-a-bounded-array.java
@@ -24,71 +24,110 @@
 // }
 
 
-class Solution {
-    public int maxValue(int n, int index, int maxSum) {
-       int l = index; // Number of elements in left part
-        int r = n-index-1; // Number of elements in right part
+// class Solution {
+//     public int maxValue(int n, int index, int maxSum) {
+//        int l = index; // Number of elements in left part
+//         int r = n-index-1; // Number of elements in right part
         
-        // range for nums[index]
-        long lo = 1;   
-        long high = maxSum;
+//         // range for nums[index]
+//         long lo = 1;   
+//         long high = maxSum;
         
-        long sum =0;
+//         long sum =0;
         
-        // final answer is stored in res
-        long res =0;
+//         // final answer is stored in res
+//         long res =0;
         
         
-        while(lo <= high)
-        {
-            long mid = lo + (high-lo)/2;
-            long ls =0; // Left Part sum
-            long rs = 0; // Right Part Sum
-            sum = mid;
+//         while(lo <= high)
+//         {
+//             long mid = lo + (high-lo)/2;
+//             long ls =0; // Left Part sum
+//             long rs = 0; // Right Part Sum
+//             sum = mid;
             
-            long m = mid-1;  // used in finding left sum and right sum
+//             long m = mid-1;  // used in finding left sum and right sum
             
-            if(r > 0)
-            {
-                if(r<=m)   
-                {   /*
-                    When right part is filled with decreasing order natural numbers
-                    then use this formula
-                    */
-                    rs = m*(m+1)/2 - (m-r)*(m-r+1)/2;
-                }else
-                    /*
-                    When right part is filled with 1's then use this formula
-                    */
-                    rs = m*(m+1)/2 + 1*(r-m);
-            }
-            if(l>0)
-            {
-            if(l<=m)
+//             if(r > 0)
+//             {
+//                 if(r<=m)   
+//                 {   /*
+//                     When right part is filled with decreasing order natural numbers
+//                     then use this formula
+//                     */
+//                     rs = m*(m+1)/2 - (m-r)*(m-r+1)/2;
+//                 }else
+//                     /*
+//                     When right part is filled with 1's then use this formula
+//                     */
+//                     rs = m*(m+1)/2 + 1*(r-m);
+//             }
+//             if(l>0)
+//             {
+//             if(l<=m)
                 
-                /*
-                9+8+7+6 = (1+2+3+4+5+.......+8+9) - (1+2+3+4+5)
-                */
-                ls = m*(m+1)/2 - (m-l)*(m-l+1)/2;
-            else
+//                 /*
+//                 9+8+7+6 = (1+2+3+4+5+.......+8+9) - (1+2+3+4+5)
+//                 */
+//                 ls = m*(m+1)/2 - (m-l)*(m-l+1)/2;
+//             else
                      
-                ls = m*(m+1)/2 + 1*(l-m);
-            }
-            // sum = left part + mid element + right part
-            sum = sum +ls +rs;
+//                 ls = m*(m+1)/2 + 1*(l-m);
+//             }
+//             // sum = left part + mid element + right part
+//             sum = sum +ls +rs;
             
-            if(sum <= maxSum)
-            {   res = Math.max(res,mid);  // Storing the maximum nums[index] 
-                //System.out.print(res);
-                lo = mid+1;
-            }else{
-                high = mid-1;
-            }
+//             if(sum <= maxSum)
+//             {   res = Math.max(res,mid);  // Storing the maximum nums[index] 
+//                 //System.out.print(res);
+//                 lo = mid+1;
+//             }else{
+//                 high = mid-1;
+//             }
             
             
+//         }
+        
+//         return (int)res;
+        
+//     }   
+//     }
+
+
+class Solution {
+    long calcSum(long n) { return (n * (n + 1)) / 2; }
+    long getSum(int index, int val, int len)
+    {
+        int expectedLen = val - 1;
+        int actualLen = len;
+        long sum = 0;
+        if (expectedLen > actualLen) sum = calcSum(expectedLen) - calcSum(val - actualLen - 1);
+        else
+        {
+            int extraLen = actualLen - expectedLen;
+            long extraSum = extraLen * 1;
+            sum = calcSum(expectedLen) + extraSum;
         }
-        
-        return (int)res;
-        
-    }   
+        return sum;
     }
+    long getSumOfArray(int n, int index, int val)
+    {
+        //1, 1, 1, (val - 2), (val - 1), val, (val - 1), (val - 2), 1, 1, 1
+        long leftSum = getSum(index, val, index);
+        long rightSum = getSum(index, val, n - index - 1);
+        return (val + leftSum + rightSum);
+    }
+    public int maxValue(int n, int index, int maxSum) 
+    {
+        int low = 1, high = maxSum;
+        int ans = -1;
+        while(low <= high)
+        {
+            int mid = low + (high - low) / 2; 
+            long sum =  getSumOfArray(n, index, mid);
+            if (sum <= maxSum) { ans = mid; low = mid + 1; }
+            else high = mid - 1;
+        }
+        return ans;
+    }
+}
